1ee9af9065d7c5c34d572cdfa31a0a0c399df4f1
peterSLDS <peter.rumpel@gmx.net>
2022-12-02 13:00:54 +0100

    Update Notebook

--- functions.py
+++ functions.py
@@ -1,10 +1,5 @@
 ## Python Functions needed for Calibration Analysis
 
-# level = 0.1
-# uncertainty = "resampling"
-# type = "confidence"
-# m=100
-
 def calc_prep(x, y, level, m, uncertainty, type):
     import numpy as np
     import pandas as pd
@@ -32,7 +27,7 @@ def calc_prep(x, y, level, m, uncertainty, type):
     # Compute Values for Uncertainty regions
     # Resampling
     if uncertainty == "resampling":
-        z = np.unique(x)
+        z = np.sort(x)
         z_df = pd.DataFrame({'Z': z})
         x_df = pd.DataFrame({'X': x})
         for l in range(1, m + 1):
@@ -51,8 +46,8 @@ def calc_prep(x, y, level, m, uncertainty, type):
             iso_bs.fit(x_resample, y_resample)
             x_uni = np.unique(x_resample)
             isofit_bs = np.array(iso_bs.predict(x_uni))
-            bs_row = pd.DataFrame({'Z': x_uni, 'fit_bs' + str(l): isofit_bs})
-            bs_row = bs_row.sort_values('Z', axis=0, ascending=True)
+            bs_inter = np.interp(x=z, xp=x_uni, fp=isofit_bs, left=0, right=1)
+            bs_row = pd.DataFrame({'Z': z, 'fit_bs' + str(l): bs_inter})
             z_df = z_df.merge(bs_row, on='Z', how='left')
         z_df = z_df.T
         z_df = z_df.rename(columns=z_df.iloc[0]).drop(z_df.index[0])
@@ -63,6 +58,7 @@ def calc_prep(x, y, level, m, uncertainty, type):
 
     elif uncertainty == "discrete":
         # Discrete Asymptotics
+        # Only consistency allowed, because confidence doesn't converege (Dimitiradis Online Appendix p.10)
         q_snd = norm.ppf(q=1 - (level / 2))
         z, c = np.unique(x, return_counts=True)
         uni_count = pd.DataFrame({'Unique': z, 'Count': c})
@@ -72,18 +68,61 @@ def calc_prep(x, y, level, m, uncertainty, type):
         uni_count['UB'] = uni_count['Unique'] + uni_count['frac']
         uni_count.loc[(uni_count.LB < 0), 'LB'] = 0
         uni_count.loc[(uni_count.UB > 1), 'UB'] = 1
-        quants = uni_count.drop(['Unique', 'Count', 'frac'], axis=1)
-    elif uncertainty == "contiuous":
-        print("Not yet available")
+        quants = pd.DataFrame({'LB': np.interp(x=x, xp=z, fp=uni_count.loc[:, 'LB']),
+                               'UB': np.interp(x=x, xp=z, fp=uni_count.loc[:, 'UB'])})
+        #quants = uni_count.drop(['Unique', 'Count', 'frac'], axis=1)
+    elif uncertainty == "continuous":
+        print("Caution: Density Estimation is not bounded!")
+        def density_estimation(x_eval):
+            from sklearn.neighbors import KernelDensity
+            x_reshape = x_eval.reshape(-1,1)
+            kde = KernelDensity()
+            kde.fit(x_reshape)
+            density = kde.score_samples(x_reshape)
+            return(np.exp(density))
+        def qchern(q_eval):
+            from scipy.interpolate import interp1d
+            chern_x = np.array([0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59,
+                0.60, 0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69,
+                0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79,
+                0.80, 0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89,
+                0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99,
+                0.991, 0.992, 0.993, 0.994, 0.995, 0.996, 0.997, 0.998, 0.999, 0.9999])
+            chern_y = np.array([0.000000, 0.013187, 0.026383, 0.039595, 0.052830,
+                    0.066096, 0.079402, 0.092757, 0.106168, 0.119645,
+                    0.133196, 0.146831, 0.160560, 0.174393, 0.188342,
+                    0.202418, 0.216633, 0.230999, 0.245530, 0.260242,
+                    0.275151, 0.290274, 0.305629, 0.321238, 0.337123,
+                    0.353308, 0.369821, 0.386694, 0.403959, 0.421656,
+                    0.439828, 0.458525, 0.477804, 0.497731, 0.518383,
+                    0.539855, 0.562252, 0.585706, 0.610378, 0.636468,
+                    0.664235, 0.694004, 0.726216, 0.761477, 0.800658,
+                    0.845081, 0.896904, 0.960057, 1.043030, 1.171530,
+                    1.189813, 1.209897, 1.232241, 1.257496, 1.286659,
+                    1.321370, 1.364637, 1.423026, 1.516664, 1.784955])
+            chern_fct = interp1d(x=chern_x, y=chern_y)
+            q_chern = chern_fct(q_eval)
+            return q_chern
+        z, c = np.unique(x, return_counts=True)
+        density = density_estimation(x_eval=z)
+        uni_count = pd.DataFrame({'Unique': z, 'Count': c, 'Density': density})
+        qchern = qchern(q_eval=1-(level/2))
+        uni_count['frac'] = (pow(uni_count['Unique'] * (1 - uni_count['Unique']) / (2*len(z)*uni_count['Density']), (1/3)))*qchern
+        uni_count['LB'] = uni_count['Unique'] - uni_count['frac']
+        uni_count['UB'] = uni_count['Unique'] + uni_count['frac']
+        uni_count.loc[(uni_count.LB < 0), 'LB'] = 0
+        uni_count.loc[(uni_count.UB > 1), 'UB'] = 1
+        quants = pd.DataFrame({'LB': np.interp(x=x, xp=z, fp=uni_count.loc[:, 'LB']),
+                               'UB': np.interp(x=x, xp=z, fp=uni_count.loc[:, 'UB'])})
+
+
     else:
         print("Please choose from 'resampling', 'discrete' or 'continuous'")
 
     quants = quants.to_numpy()
     iso_df = pd.DataFrame({'X': x, 'Y': y, 'Fit': isofit})
-    uncert_df = pd.DataFrame({'Z':z, 'LB': quants[:, 0], 'UB': quants[:, 1]})
+    uncert_df = pd.DataFrame({'Z': z, 'LB': quants[:, 0], 'UB': quants[:, 1]})
     return iso_df, uncert_df
-
-#iso_out, uncert_out = calc_prep(x=x, y=y, level=0.1, m=100, uncertainty='resampling', type='confidence')
 
 def plot_diagram(iso_data, uncert_data):
     import matplotlib.pyplot as plt
@@ -112,8 +151,6 @@ def plot_diagram(iso_data, uncert_data):
     plt.grid()
     plt.show()
 
-#plot_diagram(iso_data=iso_out, uncert_data=uncert_out)
-
 def calc_Score_Decomp(iso_data):
     import numpy as np
     import pandas as pd
@@ -141,5 +178,3 @@ def calc_Score_Decomp(iso_data):
         CORP_Decomp = CORP_Decomp.astype('float64').transpose().round(5)
         CORP_Decomp.columns = ['Value']
         return CORP_Decomp
-
-#calc_Score_Decomp(iso_data=iso_out)
